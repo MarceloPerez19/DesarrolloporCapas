48e8a04593d476d5d418de92b2b0655c84968102
user <user@USER-PC>
2020-12-10 11:47:46 -0500

    Agregar archivos de proyecto.

---
+++ CapaLogica/uTILIDADES.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapaLogica
+{
+    class uTILIDADES
+    {
+        public static string getMes(int mes)
+        {
+            string aux = "";
+            switch (mes)
+            {
+                case 1:
+                    aux = "enero";break;
+                case 2:
+                    aux = "frebrero";break;
+                case 3:
+                    aux = "Marzo";break;
+                case 4:
+                    aux = "Abril"; break;
+                case 5:
+                    aux = "Mayo"; break;
+                case 6:
+                    aux = "Junio"; break;
+                case 7:
+                    aux = "Julio"; break;
+                case 8:
+                    aux = "Agosto"; break;
+                case 9:
+                    aux = ""; break;
+                case :
+                    aux = ""; break;
+                case :
+                    aux = ""; break;
+                case :
+                    aux = ""; break;
+
+            }
+        }
+
+    }
+}
